d41be7ceafcc4e6278d2eb99a94c6154c9f4096d
tsu-nera <fox10225fox@gmail.com>
2018-07-11 16:27:48 +0900

    プロフィール関係のボタンをドロップダウンにまとめた

--- app/assets/javascripts/materialize/custom.js
+++ app/assets/javascripts/materialize/custom.js
@@ -1,10 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-});
-
-// Or with jQuery
-
 $(document).ready(function(){
     $('select').formSelect();
 });
+
+$('.dropdown-trigger').dropdown();
