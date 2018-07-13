5068cd034d3691768cfb3e3fd892d34c2049b3f1
tsu-nera <fox10225fox@gmail.com>
2018-07-13 21:41:19 +0900

    change navigaton color

--- app/assets/javascripts/fullpage-options.js
+++ app/assets/javascripts/fullpage-options.js
@@ -1,5 +1,5 @@
 new fullpage('#fullpage', {
-  navigation: true,
-  anchors: ["page1", "page2", "page3", "page4", "page5"],
-  licenseKey: "OPEN-SOURCE-GPLV3-LICENSE"
+    navigation: true,
+    anchors: ["page1", "page2", "page3", "page4", "page5"],
+    licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
 });
